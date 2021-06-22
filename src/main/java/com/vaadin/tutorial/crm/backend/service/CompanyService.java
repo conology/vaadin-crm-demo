93fda08294be1324919533df1aecc2bce870400c
medio <bojan.ivanovic@conology.onmicrosoft.com>
2021-06-22 12:23:17 +0200

    Initial commit

---
+++ src/main/java/com/vaadin/tutorial/crm/backend/service/CompanyService.java
@@ -0,0 +1,23 @@
+package com.vaadin.tutorial.crm.backend.service;
+
+import com.vaadin.tutorial.crm.backend.entity.Company;
+import com.vaadin.tutorial.crm.backend.repository.CompanyRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+//backend copied, code
+@Service
+public class CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public List<Company> findAll() {
+        return companyRepository.findAll();
+    }
+
+}
